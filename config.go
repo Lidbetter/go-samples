228361d9ede5d76618dd3dcf489e6339478477b4
Robin <robin@lidbetter.me>
2021-01-06 08:31:39 -0500

    better config env fetcher

--- config.go
+++ config.go
@@ -1,41 +1,73 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"os"
 	"reflect"
 	"strconv"
+
+	"github.com/hashicorp/go-multierror"
 )
 
-type Config struct {
-	Debug           bool   `env:"DEBUG"`
-	Environment     string `env:"APP_ENV" required:"true"`
-	DbConnectionDSN string `env:"DATABASE_DSN" required:"true"`
-}
+func FillEnvTags(confStructPtr interface{}) error {
+	value := reflect.ValueOf(confStructPtr)
+	if value.Kind() != reflect.Ptr {
+		return errors.New("must pass a pointer, not a value")
+	}
+	if value.IsNil() {
+		return errors.New("must pass a non nil pointer")
+	}
+	if value.Type().Elem().Kind() != reflect.Struct {
+		return errors.New("must pass a pointer to a struct")
+	}
 
-func MakeConfigFromEnv() (Config, error) {
-	config := Config{}
-	t := reflect.TypeOf(config)
-	v := reflect.ValueOf(&config).Elem()
-
-	// use error count instead of failing early so we get a full list of problems/missing env var in output
+	// use error count and wrapped errors instead of failing early
+	// so we get a full list of problems/missing env var in output
 	errorCount := 0
-	for i := 0; i < t.NumField(); i++ {
-		f := v.Field(i)
-		if !f.CanSet() {
-			errorCount++
-			log.Printf("config: unable to set %s", t.Field(i).Name)
+	var errorChain error
+
+	numberKindBitSize := map[reflect.Kind]int {
+		reflect.Int:    0, // auto
+		reflect.Uint:   0, // auto
+		reflect.Int8:   8,
+		reflect.Uint8:  8,
+		reflect.Int16:  16,
+		reflect.Uint16: 16,
+		reflect.Int32:  32,
+		reflect.Uint32: 32,
+		reflect.Int64:  64,
+		reflect.Uint64: 64,
+	}
+
+	isTruthy := func(str string) bool {
+		return str != "" && str != "0" && str != "false" && str != "FALSE"
+	}
+
+	direct := value.Elem() // already checked its of kind ptr
+	directType := direct.Type()
+
+	for i := 0; i < direct.NumField(); i++ {
+		tag := directType.Field(i).Tag
+		tagEnvName := tag.Get("env")
+		if tagEnvName == "" {
 			continue
 		}
 
-		tag := t.Field(i).Tag.Get("env")
-		required := t.Field(i).Tag.Get("required")
-		envVar, envVarSet := os.LookupEnv(tag)
-
-		if (required == "true" || required == "1") && !envVarSet {
+		f := direct.Field(i)
+		if !f.CanSet() {
 			errorCount++
-			log.Printf("config: missing environment variable %s", tag)
+			errorChain = multierror.Append(errorChain, fmt.Errorf("unable to set %s of type %s", directType.Field(i).Name, f.Kind(),))
+			continue
+		}
+
+		envRequiredVal := tag.Get("required")
+		envVar, envVarSet := os.LookupEnv(tagEnvName)
+
+		if !envVarSet && isTruthy(envRequiredVal) {
+			errorCount++
+			errorChain = multierror.Append(errorChain, fmt.Errorf("unable to set %s of type %s, required environment variable '%s' missing",  directType.Field(i).Name, f.Kind(), tagEnvName))
 			continue
 		}
 
@@ -43,38 +75,53 @@ func MakeConfigFromEnv() (Config, error) {
 		case reflect.String:
 			f.SetString(envVar)
 		case reflect.Bool:
-			f.SetBool(envVar == "true" || envVar == "1")
+			f.SetBool(isTruthy(envVar))
+		case reflect.Int8: fallthrough
+		case reflect.Int16: fallthrough
+		case reflect.Int32: fallthrough
+		case reflect.Int64: fallthrough
 		case reflect.Int:
-		case reflect.Int8:
-		case reflect.Int16:
-		case reflect.Int32:
-		case reflect.Int64:
-			asInt, asIntErr := strconv.ParseInt(envVar, 10, 0)
+			asInt, asIntErr := strconv.ParseInt(envVar, 10, numberKindBitSize[f.Kind()])
 			if asIntErr != nil {
 				errorCount++
-				log.Printf("config: unable to convert %s to int64", envVar)
+				errorChain = multierror.Append(errorChain, fmt.Errorf("unable to set %s of type %s from %s: %w", directType.Field(i).Name, f.Kind(), tagEnvName, asIntErr))
 				continue
 			}
 			f.SetInt(asInt)
+		case reflect.Uint8: fallthrough
+		case reflect.Uint16: fallthrough
+		case reflect.Uint32: fallthrough
+		case reflect.Uint64: fallthrough
+		case reflect.Uint:
+			asUint, asUintErr := strconv.ParseUint(envVar, 10, numberKindBitSize[f.Kind()])
+			if asUintErr != nil {
+				errorCount++
+				errorChain = multierror.Append(errorChain, fmt.Errorf("unable to set %s of type %s from %s: %w", directType.Field(i).Name, f.Kind(), tagEnvName, asUintErr))
+				continue
+			}
+			f.SetUint(asUint)
 		default:
 			errorCount++
-			log.Printf("config: unable to set %s of type %s", t.Field(i).Name, f.Kind())
+			errorChain = multierror.Append(errorChain, fmt.Errorf("unable to set %s of type %s (supported)",  directType.Field(i).Name, f.Kind()))
 			continue
 		}
 	}
 
-	if errorCount > 0 {
-		return Config{}, fmt.Errorf("missing or unable to set %d config values from environment variables", errorCount)
-	}
-
-	return config, nil
+	return errorChain
 }
 
 
+type Config struct {
+	Debug           bool   `env:"DEBUG"`
+	Environment     string `env:"APP_ENV" required:"true"`
+	DbConnectionDSN string `env:"DATABASE_DSN" required:"true"`
+}
+
 func main()  {
-	config, configErr := MakeConfigFromEnv()
-	if  configErr != nil {
-		log.Fatalf("unable to make config: %v", configErr)
+	config := Config{}
+	confErr := FillEnvTags(&config)
+	if  confErr != nil {
+		log.Fatalf("unable to make config: %v", confErr)
 	}
 
 	fmt.Printf("successfuly made config: %+v", config)
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Lidbetter/go-samples
+
+go 1.15
+
+require github.com/hashicorp/go-multierror v1.1.0
